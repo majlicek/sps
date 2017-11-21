3f2d6f08602bba7aad22c71ebf9d4003b9b48985
Majlicek <chrastina.milan@gmail.com>
2017-11-21 05:12:31 +0100

    Zadanie 1

---
+++ zadanie1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+    <title>Zadanie 1</title>
+</head>
+<body>
+<form class="form" method="post" action="">
+
+    <label for=\"volba1\">1</label>
+    <input type="radio" value="1" name="volbaRadio" id="volba1">
+    <label for=\"volba2\">2</label>
+    <input type="radio" value="2" name="volbaRadio" id="volba2">
+
+        <input type="submit" value="Zobraz info">
+</form>
+
+</body>
+</html>
